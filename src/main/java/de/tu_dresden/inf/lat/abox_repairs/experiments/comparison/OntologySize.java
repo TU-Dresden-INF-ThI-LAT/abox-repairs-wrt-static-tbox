bf9af38197d206334c1ca6b1a018f4792b9ec262
Patrick Koopmann <p.k.koopmann@vu.nl>
2023-06-27 15:47:25 +0200

    ontology statistics

--- src/main/java/de/tu_dresden/inf/lat/abox_repairs/experiments/comparison/OntologySize.java
+++ src/main/java/de/tu_dresden/inf/lat/abox_repairs/experiments/comparison/OntologySize.java
@@ -1,8 +1,10 @@
 package de.tu_dresden.inf.lat.abox_repairs.experiments.comparison;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.AxiomType;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.parameters.Imports;
 
 import java.io.File;
 
@@ -10,6 +12,10 @@ public class OntologySize {
     public static void main(String[] args) throws OWLOntologyCreationException {
         OWLOntology ontology = OWLManager.createOWLOntologyManager().loadOntologyFromOntologyDocument(new File(args[0]));
 
-        System.out.println(ontology.getAxiomCount());
+        //System.out.println(ontology.getAxiomCount());
+        System.out.println(ontology.getLogicalAxiomCount()+" "
+                + ontology.getABoxAxioms(Imports.INCLUDED).size() + " "
+                + ontology.getTBoxAxioms(Imports.INCLUDED).size()  );
     }
+
 }
